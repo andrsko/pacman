786e9314575704258c0efe6b580b3381bcacc306
Andrii Savchenko <Andrii.Savchenko.-ND@disney.com>
2023-04-18 15:35:35 +0300

    Match with lesson's result

--- main.py
+++ main.py
@@ -8,21 +8,19 @@ screen = pygame.display.set_mode((width, height))
 violet = (50, 20, 64)
 screen.fill(violet)
 pygame.display.set_caption('Pacman')
-pacman = pygame.image.load("Pacman.png")
-pacman = pygame.transform.scale(pacman, (50, 50))
 
 # початкові координати
-x = 100
-y = 100
-
+x = 300
+y = 400
+pacman = pygame.image.load("Pacman.png")
+pacman = pygame.transform.scale(pacman, (50, 50))
 screen.blit(pacman, (x, y))
-pygame.display.update()
-while True:
 
+
+while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
-
     keys = pygame.key.get_pressed()
     if keys[pygame.K_RIGHT]:
         x = x + 0.1
@@ -34,6 +32,6 @@ while True:
         y = y + 0.1
 
     screen.fill(violet)
-    screen.blit(pacman, (x, y))
     walls.get().draw(screen)
+    screen.blit(pacman, (x, y))
     pygame.display.update()
